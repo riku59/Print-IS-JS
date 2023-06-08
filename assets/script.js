c589fc5f831cc51a14d739369c8cdbd2fe242046
riku59 <123378420+riku59@users.noreply.github.com>
2023-06-08 13:39:35 +0200

    texte et image qui change au clic. problème sur le changement de dot

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,51 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-//étape 2
+//    étape 2 Ajoutez des Event Listeners sur les flèches
 let slide = 0;
 const banner = document.getElementById("banner");
 const precedent = document.querySelector(".arrow_left");
 const suivant = document.querySelector(".arrow_right");
+const bannerImg = document.querySelector(".banner-img");
+const texte = document.querySelector("#banner p");
 
+// console.log(slides[0]);
 precedent.addEventListener("click", () => {
   slide = (slide - 1 + slides.length) % slides.length; // change "slide" lorsque la fleche de gauche est cliqué
+  texte.innerHTML = slides[slide].tagLine;
+  bannerImg.src = `./assets/images/slideshow/${slides[slide].image}`;
   console.log(slide);
   console.log("left");
 });
 
 suivant.addEventListener("click", () => {
   slide = (slide + 1) % slides.length; //change "slide" lorsque la fleche de droite est cliqué
+  texte.innerHTML = slides[slide].tagLine; // change le texte par rapport a la valeur de slide.
+  bannerImg.src = `./assets/images/slideshow/${slides[slide].image}`; // change l'image par rapport a la valeur de slide.
   console.log(slide);
   console.log("right");
 });
 
-//étape 3 :
+//    étape 3 : Ajoutez des bullet points au slider
+
+//Création des dots.
+
+//création d'une constante "dots" qui cible la division ".dots"
 const dots = document.querySelector(".dots");
+
+slides.forEach((dotSelect, index) => {
+  // pour chaque slides, tu me fait :
+  const dot = document.createElement("div"); //création d'une constante dot en créant une division
+  dot.classList.add("dot"); // ajout de la classe "dot"
+  dots.appendChild(dot); // ajoute une division avec la classe dot
+});
+// dot.classList.add("dot_selected"); //ajoute la classe dot_selected a toute les dots créé
+
+// console.log(dotSelect);
+const allDot = document.querySelectorAll(".dot");
+console.log(allDot);
+allDot.forEach((dot, index) => {
+  dot.classList.toggle("dot_selected", index === slide);
+});
+
+// étape4 Modifiez le slide au clic sur le bouton
